fc20fcd291e6a897a3984528122526573490433a
Nguyễn Minh Công <108465765+atsvn123@users.noreply.github.com>
2026-06-27 18:09:37 +0700

    ok

--- app/ObjC/Controllers/SCNetworkViewController.m
+++ app/ObjC/Controllers/SCNetworkViewController.m
@@ -10,8 +10,14 @@
         NSArray *vals=@[self.config.proxyType,self.config.proxyHost,@(self.config.proxyPort).stringValue,self.config.proxyUser];
         UITableViewCell *c=[self cellWithTitle:titles[i.row-1] detail:nil]; c.accessoryView=[self textFieldWithText:vals[i.row-1] placeholder:@"" tag:100+i.row keyboard:i.row==3?UIKeyboardTypeNumberPad:UIKeyboardTypeDefault]; return c;
     }
-    if (i.section == 1) return [self switchCellWithTitle:@[@"Ẩn Proxy",@"Ẩn VPN",@"Ẩn Jailbreak"][i.row] on:@[@(self.config.hideProxy),@(self.config.hideVPN),@(self.config.hideJailbreak)][i.row].boolValue action:@selector(toggleAnti:)];
-    return [self switchCellWithTitle:@[@"Spoof IDFA",@"Spoof IDFV",@"Spoof Battery"][i.row] on:@[@(self.config.spoofIDFA),@(self.config.spoofIDFV),@(self.config.spoofBattery)][i.row].boolValue action:@selector(toggleIDs:)];
+    if (i.section == 1) {
+        NSArray *titles = @[@"Ẩn Proxy", @"Ẩn VPN", @"Ẩn Jailbreak"];
+        NSArray *values = @[@(self.config.hideProxy), @(self.config.hideVPN), @(self.config.hideJailbreak)];
+        return [self switchCellWithTitle:titles[i.row] on:[values[i.row] boolValue] action:@selector(toggleAnti:)];
+    }
+    NSArray *titles = @[@"Spoof IDFA", @"Spoof IDFV", @"Spoof Battery"];
+    NSArray *values = @[@(self.config.spoofIDFA), @(self.config.spoofIDFV), @(self.config.spoofBattery)];
+    return [self switchCellWithTitle:titles[i.row] on:[values[i.row] boolValue] action:@selector(toggleIDs:)];
 }
 - (void)saveAndReload { for (UITableViewCell *c in self.tableView.visibleCells) if ([c.accessoryView isKindOfClass:UITextField.class]) { UITextField *t=(id)c.accessoryView; if(t.tag==101) self.config.proxyType=t.text; if(t.tag==102) self.config.proxyHost=t.text; if(t.tag==103) self.config.proxyPort=t.text.integerValue; if(t.tag==104) self.config.proxyUser=t.text; } [self.config save]; }
 - (void)toggleProxy:(UISwitch*)s{self.config.proxyEnabled=s.on;[self.config save];}
